4904482a90d7162dcbbdbf6aa7c96285da2c37d1
mufidat3250 <wahabmufidat1919@gmail.com>
2022-12-05 16:44:34 +0100

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br">
+    jjjj
+    </div>
+  )
+}
+
+export default App
